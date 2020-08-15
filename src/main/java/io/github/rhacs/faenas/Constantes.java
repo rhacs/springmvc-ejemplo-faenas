b9000004d463376a877a9b393201df6c68aa0eb1
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-15 11:37:07 -0400

    added constants for Detalles sequence and id

--- src/main/java/io/github/rhacs/faenas/Constantes.java
+++ src/main/java/io/github/rhacs/faenas/Constantes.java
@@ -47,23 +47,34 @@ public class Constantes {
      */
     public static final String SECUENCIA_FAENAS = TABLA_FAENAS + "_seq";
 
+    /**
+     * Nombre de la secuencia reponsable de generar los valores para el
+     * identificador numérico de los {@link Detalle}s
+     */
+    public static final String SECUENCIA_DETALLES = TABLA_DETALLES + "_seq";
+
     // Columnas
     // -----------------------------------------------------------------------------------------
 
     /**
-     * Nombre del identificador numérico para la tabla {@link Proveedor}
+     * Nombre del identificador numérico para la tabla de {@link Proveedor}es
      */
     public static final String PROVEEDOR_ID = "proveedor_id";
 
     /**
-     * Nombre del identificador numérico para la tabla {@link Item}
+     * Nombre del identificador numérico para la tabla de {@link Item}s
      */
     public static final String ITEM_ID = "item_id";
 
     /**
-     * Nombre del identificador numérico para la tabla {@link Faena}
+     * Nombre del identificador numérico para la tabla de {@link Faena}s
      */
     public static final String FAENA_ID = "faena_id";
+
+    /**
+     * Nombre del identificador numérico para la tabla de {@link Detalle}s
+     */
+    public static final String DETALLE_ID = "detalle_id";
 
     // Constructores
     // -----------------------------------------------------------------------------------------
