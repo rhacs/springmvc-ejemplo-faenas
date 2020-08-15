1a5b4c5fbeedbc90348c23ee9f8ddee81fe2a46e
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-15 17:00:55 -0400

    added method verFormulario

--- src/main/java/io/github/rhacs/faenas/controladores/ItemsController.java
+++ src/main/java/io/github/rhacs/faenas/controladores/ItemsController.java
@@ -1,6 +1,7 @@
 package io.github.rhacs.faenas.controladores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.github.rhacs.faenas.modelos.Item;
@@ -63,6 +65,40 @@ public class ItemsController {
 
         // Devolver vista
         return "items";
+    }
+
+    /**
+     * Muestra el formulario para agregar o editar un registro
+     *
+     * @param itemid identificador numérico del {@link Item}
+     * @param modelo objeto {@link Model} que contiene el modelo de la vista
+     * @return un objeto {@link String} con la respuesta a la solicitud
+     */
+    @GetMapping(path = { "/add", "/{itemid:^[0-9]+$}/edit" })
+    public String verFormulario(@PathVariable Optional<Long> itemid, Model modelo) {
+        // Inicializar un nuevo item
+        Item item = new Item();
+
+        // Verificar si el itemid está presente en la URI
+        if (itemid.isPresent()) {
+            // Buscar información del Item
+            Optional<Item> itemExistente = itemsRepositorio.findById(itemid.get());
+
+            // Verificar si el item no existe
+            if (!itemExistente.isPresent()) {
+                // Redireccionar
+                return "redirect:/items?wrongid=" + itemid.get();
+            }
+
+            // Reemplazar el item
+            item = itemExistente.get();
+        }
+
+        // Agregar item al modelo
+        modelo.addAttribute("item", item);
+
+        // Devolver vista
+        return "items.form";
     }
 
 }
