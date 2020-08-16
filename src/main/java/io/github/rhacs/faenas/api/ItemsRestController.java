987fe8f639ba33133b0a953082c2b241c89abd3a
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-15 21:33:08 -0400

    added method obtenerItem

--- src/main/java/io/github/rhacs/faenas/api/ItemsRestController.java
+++ src/main/java/io/github/rhacs/faenas/api/ItemsRestController.java
@@ -1,6 +1,7 @@
 package io.github.rhacs.faenas.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,11 +9,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.rhacs.faenas.excepciones.ElementNotFoundException;
+import io.github.rhacs.faenas.modelos.ErrorResponse;
 import io.github.rhacs.faenas.modelos.Item;
 import io.github.rhacs.faenas.repositorios.ItemsRepositorio;
 
@@ -45,17 +51,53 @@ public class ItemsRestController {
      * @return un objeto {@link List} con la respuesta
      */
     @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
     public List<Item> obtenerListado(HttpServletRequest request) {
-        logger.trace("Solicitud GET: {}", request.getRequestURI());
-        logger.trace("Obteniendo el listado de items");
+        logger.info("[API] Solicitud POST: {}", request.getRequestURI());
 
         // Obtener listado de items
         List<Item> items = itemsRepositorio.findAll();
 
-        logger.trace("Listado de Items: {}", items);
+        logger.info("[API] Listado de Items: {}", items);
 
         // Devolver listado
         return items;
+    }
+
+    /**
+     * Muestra la información del {@link Item} solicitado por URI
+     * 
+     * @param id      identificador numérico del {@link Item}
+     * @param request objeto {@link HttpServletRequest} que contiene la información
+     *                de la solicitud que le hace el cliente al servlet
+     * @return un objeto {@link Item} con la respuesta
+     */
+    @GetMapping(path = "/{id:^[0-9]+$}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Item obtenerItem(@PathVariable Long id, HttpServletRequest request) {
+        logger.info("[API] Solicitud POST: {}", request.getRequestURI());
+
+        // Obtener información del Item solicitado, lanzar excepción
+        Optional<Item> item = itemsRepositorio.findById(id);
+
+        // Verificar si existe
+        if (item.isPresent()) {
+            logger.info("[API] Mostrando información del Item solicitado: {}", item.get());
+
+            // Devolver objeto
+            return item.get();
+        }
+
+        logger.info("[API] Objeto no encontrado para el identificador {}", id);
+
+        // Crear respuesta de error
+        ErrorResponse response = new ErrorResponse();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage("El objeto con el identificador " + id + " no existe");
+        response.addError("id", "El objeto no existe", id);
+
+        // Lanzar excepción
+        throw new ElementNotFoundException(response);
     }
 
 }
