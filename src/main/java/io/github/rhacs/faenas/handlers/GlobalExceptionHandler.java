5b817b1c5109853d875fd85d1eadf47e79a849f8
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-15 21:20:44 -0400

    added global exception handler

---
+++ src/main/java/io/github/rhacs/faenas/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package io.github.rhacs.faenas.handlers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import io.github.rhacs.faenas.excepciones.ElementNotFoundException;
+
+@RestControllerAdvice(basePackages = { "io.github.rhacs.faenas.api" })
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = ElementNotFoundException.class)
+    protected ResponseEntity<?> handleElementNotFoundException(ElementNotFoundException e) {
+        return ResponseEntity.status(e.getResponse().getStatus()).body(e.getResponse());
+    }
+
+}
