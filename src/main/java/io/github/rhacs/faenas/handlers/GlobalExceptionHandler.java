7bddf78822e0d0af7d4ca08de81917dd8c0cf7ef
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-15 21:32:21 -0400

    changed response type

--- src/main/java/io/github/rhacs/faenas/handlers/GlobalExceptionHandler.java
+++ src/main/java/io/github/rhacs/faenas/handlers/GlobalExceptionHandler.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import io.github.rhacs.faenas.excepciones.ElementNotFoundException;
+import io.github.rhacs.faenas.modelos.ErrorResponse;
 
 @RestControllerAdvice(basePackages = { "io.github.rhacs.faenas.api" })
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = ElementNotFoundException.class)
-    protected ResponseEntity<?> handleElementNotFoundException(ElementNotFoundException e) {
+    protected ResponseEntity<ErrorResponse> handleElementNotFoundException(ElementNotFoundException e) {
         return ResponseEntity.status(e.getResponse().getStatus()).body(e.getResponse());
     }
 
